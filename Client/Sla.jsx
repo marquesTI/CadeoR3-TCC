9c041052d6ba3ff73e74efb73a1f2f7220dfd8ff
marquesTI <gustavomattos.ti@gmail.com>
2024-06-19 12:37:19 -0300

    Criando carrinho de compras

---
+++ Client/Sla.jsx
@@ -0,0 +1,13 @@
+import Search from "./src/components/SearchBar/SearchBar";
+import Cart from "./src/components/CartButton/CartButton";
+
+function Sla() {
+  return (
+    <div>
+      <Search />
+      <Cart />
+    </div>
+  );
+}
+
+export default Sla;
---
+++ Client/src/routes/Test.jsx
@@ -0,0 +1,11 @@
+import Sla from "../components/Sla.jsx";
+
+function Test() {
+  return (
+    <>
+      <Sla />
+    </>
+  );
+}
+
+export default Test;
